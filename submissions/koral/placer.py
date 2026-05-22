08e6e4014478e829051c969712e547013667a723
Ian Titor <ianpatricktan@gmail.com>
2026-05-21 23:11:14 -0400

    small fix to placer

--- submissions/koral/placer.py
+++ submissions/koral/placer.py
@@ -14,6 +14,7 @@ from __future__ import annotations
 import math
 import random
 import time
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple
 
